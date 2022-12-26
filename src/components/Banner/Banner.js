a978cf1b12264ac6a3e9f54381f33fb0a710ab17
leonan <leonan.marques886@gmail.com>
2022-12-26 20:36:31 -0300

    feature: aula 1, atividades 7 e 8

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,13 +1,10 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     // a definição do nome da classe no header não pode ser como seria no html, pois 'class' é uma palavra reservada do js.
     return(
         <header className='banner'> 
             <img src="/images/banner.png" alt="O banner principal da página do Organo."/>
         </header>
-        
     )
 }
-
-export default Banner;
