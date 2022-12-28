184a0d63513dbe803608fe21230d50694b2fab0f
leonan <leonan.marques886@gmail.com>
2022-12-28 10:36:29 -0300

    feature: aula 3, atividade 2

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,10 +1,18 @@
 import "./TextField.css";
 
 export const TextField = (props) => {
+
+    let value = 'João Silveira'
+
+    const onTyped = (event) => {
+        value = event.target.value
+        console.log(value)
+    }
+
     return (
         <div className="text-field">
             <label> {props.label} </label>
-            <input required = {props.required}  placeholder={props.placeholder}/>
+            <input value={value}  onChange={onTyped}  required = {props.required}  placeholder={props.placeholder}/>
         </div>
     )
 }
