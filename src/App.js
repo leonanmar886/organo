2bb757f2094e64daf1b47709d59fef9461b0c48e
leonan <leonan.marques886@gmail.com>
2022-12-28 12:28:04 -0300

    feature: aula 4, atividade 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
 
@@ -12,6 +13,10 @@ function App() {
       <Form onPlayerRegistered = {
         player => setPlayers([...players, player])
       }/>
+      <Team name = "Loud"/>
+      <Team name = "Furia"/>
+      <Team name = "Faze"/>
+      <Team name = "Cloud9"/>
     </div>
   );
 }
