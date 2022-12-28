9a74b918e4900b683e7e895b36c81dd672fb4e25
leonan <leonan.marques886@gmail.com>
2022-12-28 10:14:00 -0300

    rebase: remoção do campo de time

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,12 +6,11 @@ import { Button } from '../Button/Button'
 export const Form = () => {
 
     const teams = [
-        'Loud',
-        'Liquid',
-        'OpTic Gaming',
-        'NyP', 
-        'Cloud9',
-        'Furia'
+        'Duelista',
+        'Iniciador',
+        'Controlador',
+        'Sentinela', 
+        'Coach'
     ]
 
     const toSave = (event) => {
@@ -25,9 +24,8 @@ export const Form = () => {
                 <h2>Preencha os dados para criar o card do player.</h2>
                 <TextField required = {true} label = "Nome" placeholder = "Digite o seu nome"/>
                 <TextField required = {true}  label = "Username" placeholder = "Digite o seu nome de usuário"/>
-                <TextField  required = {true} label = "Função" placeholder = "Digite a sua função"/>
                 <TextField label = "Imagem" placeholder = "Informe o endereço da sua imagem"/>
-                <DropDown label="Time" items = {teams} />
+                <DropDown label="Função" items = {teams} />
                 <Button>
                     Criar Card
                 </Button>
